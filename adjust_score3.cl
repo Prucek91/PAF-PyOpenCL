2d0e238e888fa2127eb788cf887f8b853603c79f
Prucek91 <62542135+Prucek91@users.noreply.github.com>
2022-08-08 17:14:15 +0200

    Add files.
    .

---
+++ adjust_score3.cl
@@ -0,0 +1,87 @@
+//#pragma OPENCL EXTENSION cl_intel_printf : enable
+
+int idx_(int i, int j, int N) { return (j * N) + i; }
+
+char SOF(int i, int j, int k, __global char *F, int N) {
+  if (i < N && j < N) {
+    int idx_i = j + 1;
+    int idx_j = min(k, (2 * j) - i + 1);
+    return F[idx_(idx_i, idx_j, N)];
+  } else {
+    return 0;
+  }
+}
+
+char S(int i, int j, __global char *F, int N) {
+  if (j > N) {
+    return 0;
+  }
+
+  char result = 0;
+  int k = j + 1;
+  // for (int k = j + 1; k <= N; k++ )
+  // {
+  char result1 = SOF(i, j, k, F, N) + S(j + 1, k, F, N);
+  if (result1 > result) {
+    result = result1;
+  }
+  // }
+  return result;
+}
+
+__kernel void adjust_score(__global char *c, __global char *F, const int N) {
+  int x = get_global_id(0); // x
+  int y = get_global_id(1); // y
+  // c[(y * N) + x] = (y * N )+ x; - index 2D w macierzy 1D
+  int i = x;
+  int j = y;
+
+  // podzial zadan
+  const int grid_size = 255;
+  int idx_per_thread = N / grid_size;
+
+  float t = (N / grid_size);
+  int MIN_BOUND = floor(t);
+  int MAX_BOUND = ceil(t);
+  int MIN_X_IDX = i * MIN_BOUND;
+  int MAX_X_IDX = (i * MAX_BOUND) + MAX_BOUND;
+  int MIN_Y_IDX = j * MIN_BOUND;
+  int MAX_Y_IDX = (j * MAX_BOUND) + MAX_BOUND;
+
+//  printf("Kernel i %d j %d zakres %d - %d, %d - %d \n", x, y, MIN_X_IDX,
+//         MAX_X_IDX, MIN_Y_IDX, MAX_Y_IDX);
+
+  for (int ii = MIN_X_IDX; ii <= MAX_X_IDX; ii++) {
+    for (int jj = MIN_Y_IDX; jj <= MAX_Y_IDX; jj++) {
+      i = ii;
+      j = jj;
+
+      int idx = idx_(i, j, N);
+      if (idx >= (N * N)) {
+        c[idx] = 0;
+      } else {
+
+        // zredukowany wzor rekurencyjny 3.2
+        if (j >= (N - 1)) {
+          c[idx] = 0;
+        } else {
+          char result = 0;
+
+          for (int k = j + 1; k <= N; k++) {
+            char tmp = 0;
+            int idx_F_i = j + 1;
+            int idx_F_j = min(k, (2 * j) - i + 1);
+            int idx_S_i = j + 1;
+            int idx_S_j = k;
+            tmp = F[idx_(idx_F_i, idx_F_j, N)] + c[idx_(idx_S_i, idx_S_j, N)];
+            if (result < tmp) {
+              result = tmp;
+            }
+          }
+
+          c[idx] = result;
+        }
+      }
+    }
+  }
+}
---
+++ paf_ocl.py
@@ -0,0 +1,159 @@
+import numpy as np
+import pyopencl as cl
+import pyopencl
+import time
+import pandas as pd
+from tqdm import tqdm
+import pyopencl.cltypes as cltypes
+import copy
+
+
+
+def action(N_, iter=-1):
+    prepare_data_start_time = time.time()
+    prepare_data_start_time1 = time.process_time()
+    N = N_
+    DIM = int(N + 10)
+    c = np.random.randint(low=0, high=20, size=(DIM,DIM)).astype(cltypes.char)
+    F = np.random.randint(low=0, high=20, size=(DIM,DIM)).astype(cltypes.char)
+    c = c.ravel()
+    F = F.ravel()
+
+    final = copy.deepcopy(c)
+
+    prepare_data_stop_time = time.time()
+    prepare_data_stop_time1 = time.process_time()
+
+
+    f = open('adjust_score3.cl', 'r', encoding='utf-8')
+    kernels = ''.join(f.readlines())
+    f.close()
+
+
+    #ctx = cl.create_some_context()
+    create_ctx_start_time = time.time()
+    create_ctx_start_time1 = time.process_time()
+
+    platform = pyopencl.get_platforms()[0]
+    devices = platform.get_devices()
+    #print(devices)
+    ctx = pyopencl.Context(devices)
+
+    create_ctx_stop_time = time.time()
+    create_ctx_stop_time1 = time.process_time()
+
+    ctx_queue_start_time = time.time()
+    ctx_queue_start_time1 = time.process_time()
+
+    queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
+
+    ctx_queue_stop_time = time.time()
+    ctx_queue_stop_time1 = time.process_time()
+
+
+
+    compile_kernel_start_time = time.time()
+    compile_kernel_start_time1 = time.process_time()
+    prg = cl.Program(ctx, kernels).build()
+    compile_kernel_stop_time = time.time()
+    compile_kernel_stop_time1 = time.process_time()
+
+
+
+    upload_data_start_time = time.time()#time.time()
+    upload_data_start_time1 = time.process_time()
+
+    #dev_c_matrix = cl.Buffer(ctx, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=c)
+    #dev_F_matrix = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=F)
+
+    # https://documen.tician.de/pyopencl/runtime_const.html#pyopencl.mem_flags
+    dev_c_matrix = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=c)
+    dev_F_matrix = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=F)
+    #dev_final_matrix = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=final)
+
+    upload_data_stop_time = time.time()
+    upload_data_stop_time1 = time.process_time()
+
+
+
+    opencl_start = time.time()
+    opencl_start1 = time.process_time()
+    GRID_SIZE = 255
+    evt = prg.adjust_score(queue, (255,255), (1,1), 
+        dev_c_matrix,
+        dev_F_matrix, 
+        np.int32(N),
+        #dev_final_matrix)        
+    )
+    evt.wait()
+
+    opencl_stop = time.time()
+    opencl_stop1 = time.process_time()
+
+
+
+    download_time_start = time.time()
+    download_time_start1 = time.process_time()
+
+    #cl.enqueue_copy(queue, final, dev_final_matrix).wait()
+    
+    download_time_stop = time.time()
+    download_time_stop1 = time.process_time()
+
+    #final = final.reshape((DIM,DIM))
+
+    d = {
+        'Device name': devices[0],   
+        'Dimentions': N,
+        'prepare data t' : prepare_data_stop_time - prepare_data_start_time,
+        'create ctx t' : create_ctx_stop_time - create_ctx_start_time,
+        'create command queue t' : ctx_queue_stop_time - ctx_queue_start_time,
+        'first data upload t' : upload_data_stop_time - upload_data_start_time,
+        'kernel compile t' : compile_kernel_stop_time - compile_kernel_start_time,
+        'download data time t' : download_time_stop - download_time_start,
+        'opencl execution time t' : opencl_stop - opencl_start,
+
+        'prepare data p' : prepare_data_stop_time1 - prepare_data_start_time1,
+        'create ctx p' : create_ctx_stop_time1 - create_ctx_start_time1,
+        'create command queue p' : ctx_queue_stop_time1 - ctx_queue_start_time1,
+        'first data upload p' : upload_data_stop_time1 - upload_data_start_time1,
+        'kernel compile p' : compile_kernel_stop_time1 - compile_kernel_start_time1,
+        'download data time p' : download_time_stop1 - download_time_start1,
+        'opencl execution time p' : opencl_stop1 - opencl_start1,
+        }
+
+
+    df = pd.DataFrame([d])
+    def f(x):
+        res = x
+        try:
+            res = np.round_(x, decimals=5)
+        except:
+            pass
+        return res  
+    df = df.applymap(f)
+    df.to_csv('ocl.csv', mode='a', index=False,header=False)
+    
+    if iter >= 0:
+        print('Iteracja:', iter, end='\n')
+    print(df[['Dimentions','opencl execution time t','opencl execution time p'  ]].to_string())
+    print(50 * '-')
+    #print(info, sep='  ', end='')
+    
+    return df
+
+
+
+if __name__ == '__main__':
+    TYPE = 0
+
+    if TYPE == 0:
+        action(300, iter=0)
+
+    if TYPE == 1:
+        iters = 5
+        Ns = list(np.arange(200, 2301, 100))
+        Ns.reverse()
+        for i in range(iters):
+            for n in Ns:
+                action(n)
